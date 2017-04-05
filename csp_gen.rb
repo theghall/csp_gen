6f209175b4db6996fe58526837f1582fb933a338
theghall <guategary@gmail.com>
2017-04-05 09:20:41 -0400

    Fix problem with detecting inline style vs. stylesheet

--- csp_gen.rb
+++ csp_gen.rb
@@ -2,18 +2,16 @@
 #
 require 'byebug'
 
-directives = {:default => '', :font => '', :frame_src => '', :img => '', :script => '', :stylej => ''}
+directives = {:default_src => '', :font => '', :frame_src => '', :img_src => '', :script_src => '', :style_src => ''}
 
 def get_error_type(line)
-  errors = %w{font frame image script stylesheet}
-  directive_index = {:default => :default, :font => :font, :frame => :frame_src, :image => :image, :stylesheet => :stylesheet, :script => :script}
+  errors = %w{font frame image inline script stylesheet}
+  directive_index = {:inline => :default_src, :font => :font_src, :frame => :frame_src, :image => :image_src, :stylesheet => :style_src, :script => :script_src}
 
   error_type = :unknown
-
+ 
   errors.each do |error|
     if line.include?(error)
-      error = "default" if line.include?("inline")
-
       error_type = directive_index[error.to_sym]
       break
     end
@@ -27,7 +25,9 @@ def parse_error(line)
   puts(">>>#{type}<<<") 
 end
 
-File.open('csp_error.txt','r') do |csp_file|
+fn = ARGV[0]
+
+File.open(fn,'r') do |csp_file|
 
   while line = csp_file.gets
     line.chomp!.strip!
