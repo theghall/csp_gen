09c907aa1885b7b00cc214c04d3f319d3801efe9
theghall <guategary@gmail.com>
2017-04-05 11:21:32 -0400

    Add get_filename

--- csp_gen.rb
+++ csp_gen.rb
@@ -2,11 +2,11 @@
 #
 require 'byebug'
 
-directives = {:default_src => '', :font => '', :frame_src => '', :img_src => '', :script_src => '', :style_src => ''}
+DIRECTIVES = {:default_src => '', :font_src => '', :frame_src => '', :img_src => '', :script_src => '', :style_src => ''}
 
 def get_error_type(line)
   errors = %w{font frame image inline script stylesheet}
-  directive_index = {:inline => :default_src, :font => :font_src, :frame => :frame_src, :image => :image_src, :stylesheet => :style_src, :script => :script_src}
+  directive_index = {:inline => :default_src, :font => :font_src, :frame => :frame_src, :image => :img_src, :stylesheet => :style_src, :script => :script_src}
 
   error_type = :unknown
  
@@ -20,9 +20,24 @@ def get_error_type(line)
   error_type
 end
 
+def get_filename(line)
+  # the first '' enclosed text should be the filename
+  split = line.split('\'')
+
+  filename = split[1]
+end
+
 def parse_error(line)
   type = get_error_type(line)
-  puts(">>>#{type}<<<") 
+  
+  if type != :default
+    if type != :unknown
+      file = get_filename(line)
+      DIRECTIVES[type] << file + '; '
+    end
+  else
+    # handle default_src
+  end
 end
 
 fn = ARGV[0]
@@ -32,7 +47,7 @@ File.open(fn,'r') do |csp_file|
   while line = csp_file.gets
     line.chomp!.strip!
     puts line
-    parse_error(line.chomp) unless line == ''
+    parse_error(line) unless line == ''
   end
 end
 
